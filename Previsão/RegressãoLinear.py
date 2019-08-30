fa29bf028c95344a103f55ea8b208df46a46fe73
Cássio Giehl da Rosa <cassio.giehl@gmail.com>
2019-08-29 22:44:00 -0300

    to be continue

--- Previsão/RegressãoLinear.py
+++ Previsão/RegressãoLinear.py
@@ -1,5 +1,13 @@
 #%%
 import pandas as pd
-
+from sklearn.datasets import load_boston
 
 #%%
+df = load_boston()
+print(df.keys())
+# previsores = df.data
+# classificadores = df.target
+
+#%%
+# df.feature_names
+print(df.DESCR)

a742ae8139c733b5817ade4229da3968906314bf
Cássio Giehl da Rosa <cassio.giehl@gmail.com>
2019-09-02 08:24:07 -0300

    Previsão

--- Previsão/RegressãoLinear.py
+++ Previsão/RegressãoLinear.py
@@ -1,13 +1,30 @@
 #%%
-import pandas as pd
 from sklearn.datasets import load_boston
 
 #%%
 df = load_boston()
-print(df.keys())
-# previsores = df.data
-# classificadores = df.target
+print(df.DESCR)
 
 #%%
-# df.feature_names
-print(df.DESCR)
+previsores = df.data
+classificadores = df.target
+
+#%%
+from sklearn.model_selection import train_test_split
+
+# Separando dados de treino e teste
+X_treino, X_teste, y_treino, y_teste = train_test_split(previsores, classificadores, test_size=0.3)
+
+#%%
+from sklearn.linear_model import LinearRegression
+
+regressao_linear = LinearRegression()
+regressao_linear.fit(X_treino, y_treino)
+y_pred = regressao_linear.predict(X_teste)
+print(y_pred)
+
+#%%
+from matplotlib import pyplot as plt
+
+plt.scatter(x=y_teste, y=y_pred)
+plt.show()
